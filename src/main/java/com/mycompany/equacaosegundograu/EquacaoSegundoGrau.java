a6552b7b8a964a9c486bf74b40bf6ce3304113f5
martinssluis <luishenrique.martinslima@gmail.com>
2024-02-19 00:26:59 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/equacaosegundograu/EquacaoSegundoGrau.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.equacaosegundograu;
+
+/**
+ *
+ * @author Dell
+ */
+public class EquacaoSegundoGrau {
+
+    public static void main(String[] args) {
+        
+    }
+}
